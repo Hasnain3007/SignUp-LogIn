6ed2bb4d2dd99d0b2f43c764e385f5351305fa01
Muhammad Hasnain Khatri Attari <hasnainkhatri45@gmail.com>
2024-08-15 02:29:37 -0700

    okokokoko

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,8 @@
 import {onAuthStateChanged,signOut  } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
+import {collection, addDoc,getFirestore,getDocs   } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { app } from "./files/firebase.js";
 // getting DOM values
+
 
 var input = document.getElementById('input')
 var saveBtn = document.getElementById('save')
@@ -8,41 +10,76 @@ var ul = document.getElementById('ul')
 
 //todo ka kam yaha he
 
-saveBtn.addEventListener('click', () => {
-    if (input.value == '') {
-        alert('Please Write Your ToDo')
-    } else {
-        ul.innerHTML += `
-        <div class="main2 d-flex mt-3 col-12 id='div'">
-        <li class='li' id='li'>
-        ${input.value}
-        </li>
-        <button class="edit mx-3" id='edit' >Edit</button>
-        <button class="delete" id='delete'>Delete</button>
-        </div>
-        `
-        var li = document.getElementById('li')
-        var edit = document.getElementById('edit')
-        var dlt = document.getElementById('delete')
-        var div = document.getElementById('div')
-        input.value = ''
-        edit.addEventListener('click', () => {
-            var changevalue = prompt('Enter New Value')
-            if (changevalue == '') {
-                alert('Enter Your ToDo')
-                
-            } else {
-                
-                li.innerText = changevalue
-            }
-            
-        })
-        dlt.addEventListener('click', () => {
-            div.remove()
+// saveBtn.addEventListener('click', () => {
+        // if (input.value == '') {
+        //         alert('Please Write Your ToDo')
+        //     } else {
+        //             ul.innerHTML += `
+        //             <div class="main2 d-flex mt-3 col-12 id='div'">
+        //             <li class='li' id='li'>
+        //             ${input.value}
+        //             </li>
+        //             <button class="edit mx-3" id='edit' >Edit</button>
+        //             <button class="delete" id='delete'>Delete</button>
+        //             </div>
+        //             `
+        // var li = document.getElementById('li')
+        // var edit = document.getElementById('edit')
+        // var dlt = document.getElementById('delete')
+        // var div = document.getElementById('div')
+        // input.value = ''
+        // edit.addEventListener('click', () => {
+        //         var changevalue = prompt('Enter New Value')
+        //         if (changevalue == '') {
+        //                 alert('Enter Your ToDo')
         
-        })
-    }
-})
+        //             } else {
+            
+        //                 li.innerText = changevalue
+        //             }
+        
+        //         })
+        //         dlt.addEventListener('click', () => {
+        //                 div.remove()
+            
+        //             })
+        //         }
+            // })  
+            
+            
+            const db = getFirestore(app);
+            saveBtn.addEventListener('click',async ()=>{
+
+                // setting data to fireBase
+                
+                try {
+                    const docRef = await addDoc(collection(db, "todo"), {
+                        toDo: `${input.value}`,
+                    });
+                    console.log("Document written with ID: ", docRef.id);
+                } catch (e) {
+                    console.error("Error adding document: ", e);
+                }
+                
+                input.value= ''
+
+                //  END   setting data to fireBase
+
+                gettingDataFromFireBase()
+            })
+            async function gettingDataFromFireBase() {
+                
+                const querySnapshot = await getDocs(collection(db, "todo"));
+                
+                querySnapshot.forEach((doc) => {
+                    let [dStructure] = doc.data()
+                    ul.innerHTML+=`<li>${dStructure}</li>`
+
+                    // console.log(`${doc.id} => ${doc.data()}`);
+
+                });
+            }
+            gettingDataFromFireBase()
 
 //impot karrha hu auth ko firebase.js se
 
--- files/firebase.js
+++ files/firebase.js
@@ -14,6 +14,7 @@
  const auth = getAuth(app);
  
  export{
+  app,
   auth,
   createUserWithEmailAndPassword
  }
